57ddbc9ac5b014b075e2d6c0f3a9725ed97722c0
Ole Kodehode <ole.Kodehode@gmail.com>
2025-12-16 13:57:53 +0100

    Refactoring started: sessionService to handle active sessiosn, and activeSessions json file to store active sessions between restarts.

---
+++ src/services/sessionServices.ts
@@ -0,0 +1,46 @@
+import { readFileSync, writeFileSync } from "node:fs";
+import path from "node:path";
+
+const SESSIONS_FILE = path.join(__dirname, "../data/activeSessions.json");
+
+// In-memory caching for speed
+let activeSessions: Set<string> = new Set();
+
+try {
+  const data = readFileSync(SESSIONS_FILE, "utf8");
+  const loaded = JSON.parse(data);
+  activeSessions = new Set(loaded);
+  console.log(`Loaded ${loaded.length} active sessions from file.`);
+} catch (err) {
+  console.log("Couldn't find any existing session files - Starting fresh.");
+  console.error("Error:", err);
+  activeSessions = new Set();
+}
+
+const saveSessions = () => {
+  try {
+    writeFileSync(SESSIONS_FILE, JSON.stringify([...activeSessions]));
+  } catch (err) {
+    console.error("Failed to save sessions:", err);
+  }
+};
+
+export const addSession = (sid: string): void => {
+  activeSessions.add(sid);
+  saveSessions();
+};
+
+export const hasSession = (sid: string): boolean => {
+  return activeSessions.has(sid);
+};
+
+export const removeSession = (sid: string): void => {
+  activeSessions.delete(sid);
+  saveSessions();
+};
+
+// Mostly for testing
+export const clearAllSessions = (): void => {
+  activeSessions.clear();
+  saveSessions();
+};
