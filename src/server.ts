915ddc6ef0fc89c21659a3bbfdb6c1227db12f8a
Ole Kodehode <ole.kodehode@gmail.com>
2026-01-02 11:18:18 +0100

    fix for health status and end of line to LF

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const secrets = process.env.ACCESS_SECRET && process.env.REFRESH_SECRET;
 
 if (!secrets) {
   throw new Error(
-    "Secrets needed for JWT is not set - Please make a .env file with the required secrets"
+    "Secrets needed for JWT is not set - Please make a .env file with the required secrets or run 'npm run setup'"
   );
 }
 
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
-app.get("/v1/health", healthRoutes);
+app.use("/v1/health", healthRoutes);
 
 app.use("/v1/auth", authRoutes);
 app.use("/v1/entries", backlogRoutes);
